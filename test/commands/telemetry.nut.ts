b515bf9da73f8b8a87a94653fe64b3b1fbbdb8f2
Benjamin Maggi <bmaggi@salesforce.com>
2021-04-23 13:41:24 -0300

    feat: add nut test

---
+++ test/commands/telemetry.nut.ts
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2021, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+import { expect } from 'chai';
+import { execCmd, TestSession } from '@salesforce/cli-plugins-testkit';
+import { getString } from '@salesforce/ts-types';
+import { fs } from '@salesforce/core';
+
+export interface ListApiDisplayOutput {
+  status: number;
+  result: Array<Record<string, unknown>>;
+}
+
+describe('telemetry', () => {
+  let testSession: TestSession;
+
+  before('prepare session and ensure environment variables', () => {
+    testSession = TestSession.create({});
+  });
+
+  after(async () => {
+    await testSession?.clean();
+  });
+
+  it('should enable the telemetry', () => {
+    const command = 'telemetry';
+    const result = execCmd(command, { ensureExitCode: 0 });
+    const output = getString(result, 'shellOutput.stdout').split('\n');
+    const tempDir = output[1].split(' ').slice(-1).pop().slice(0, -1);
+    const cacheDir = output[2].split(' ').slice(-1).pop().slice(0, -1);
+    expect(fs.existsSync(tempDir)).to.be.true;
+    expect(fs.existsSync(cacheDir)).to.be.true;
+    expect(output[0]).to.include('Telemetry is enabled');
+  });
+});
