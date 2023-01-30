d92b44a57c27a3d709084eebf25e5f6f3b0f11c3
Mike Donnalley <mdonnalley@salesforce.com>
2023-01-30 16:09:43 -0700

    feat: update to oclif/core v2 (#415)

--- test/helpers/myCommand.ts
+++ test/helpers/myCommand.ts
@@ -5,9 +5,9 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
-import { Flags, Config } from '@oclif/core';
+import { Flags, Command } from '@oclif/core';
 
-export class MyCommand {
+export class MyCommand extends Command {
   public static id = 'test';
   public static hidden = false;
   public static aliases = [];
@@ -17,20 +17,10 @@ export class MyCommand {
     targetusername: Flags.string(),
     targetdevhubusername: Flags.string(),
   };
-  public static args = [];
-
-  public static _base = '';
-
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  public constructor(argv: string[], config: Config) {}
-
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  public static run(): PromiseLike<any> {
-    return Promise.resolve();
-  }
+  public static args = {};
 
   // eslint-disable-next-line class-methods-use-this
-  public _run(): Promise<void> {
+  public async run(): Promise<void> {
     return Promise.resolve();
   }
 }
