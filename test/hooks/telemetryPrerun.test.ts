c16c935a5825e71c19dfe816f66e3a3fad8705fb
mshanemc <shane.mclaughlin@salesforce.com>
2023-05-16 09:47:47 -0500

    chore: pass a function the args it expects

--- test/hooks/telemetryPrerun.test.ts
+++ test/hooks/telemetryPrerun.test.ts
@@ -16,10 +16,7 @@ import { CommandExecution } from '../../src/commandExecution';
 import hook from '../../src/hooks/telemetryPrerun';
 import { MyCommand } from '../helpers/myCommand';
 
-// The hook doesn't like the stubInterface type, so just set it to any.
-/* eslint-disable @typescript-eslint/no-unsafe-argument */ // so we can use it later
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const args: any = { argv: [], Command: MyCommand };
+const args = { argv: [], Command: MyCommand, config: {} as Config };
 
 describe('telemetry prerun hook', () => {
   let sandbox: sinon.SinonSandbox;
