834930a20f54ab8a0bf80e9d38298f98e4374497
mshanemc <shane.mclaughlin@salesforce.com>
2024-06-07 14:31:14 -0500

    refactor: just one baseball game

--- src/gatherEnvs.ts
+++ src/gatherEnvs.ts
@@ -4,13 +4,6 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-
-/*
- * Copyright (c) 2023, salesforce.com, inc.
- * All rights reserved.
- * Licensed under the BSD 3-Clause license.
- * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
- */
 export const getRelevantEnvs = (): { specifiedEnvs: string[]; uniqueEnvs: string[] } => {
   const specifiedEnvs = Object.keys(process.env).filter(isOurEnv).sort();
   return {
